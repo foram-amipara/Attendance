4f1cb3e228770e5c01339f5deb21cfed86788a6c
Foram <foramamipara2@gmail.com>
2026-08-31 22:09:18 +0530

    Organize backend inside server directory

--- validators/attendanceSchema.js
+++
@@ -1,9 +0,0 @@
-const Joi=require("joi");
-
-module.exports.attendanceSchema=Joi.object({
-    subjectId: Joi.string().hex().length(24).required(),
-    type:Joi.string().valid("LECTURE","LAB").required(),
-    status:Joi.string().valid("PRESENT","ABSENT","CANCELLED").required(),
-    date:Joi.date().iso().optional(),
-});
-
